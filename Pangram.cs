b91bf1b964cafbb57b08b363fad7c6d00d5c7982
Emertat <Emertat@RAITY>
2024-08-27 16:56:48 +0330

    Add project files.

---
+++ Pangram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangram;
+public class Pangram
+{
+   public bool IsPangram(string sentence)
+    {
+        bool[] alphabet = new bool[26];
+        string str = sentence.ToLower();
+
+
+        foreach (char letter in str)
+        {
+
+            if (letter >= 'a' && letter <= 'z')
+            {
+                int index = letter - 'a'; // محاسبه ایندکس بر اساس حرف
+                alphabet[index] = true;
+            }
+        }
+
+        foreach (bool seen in alphabet)
+        {
+            if (!seen)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Pangram;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Pangram pangram=new Pangram();
+
+        // درخواست ورودی از کاربر
+        Console.WriteLine("Please enter a sentence to check if it's a pangram:");
+        string input = Console.ReadLine();
+
+     
+        bool isPangram = pangram.IsPangram(input);
+
+
+        if (isPangram)
+        {
+            Console.WriteLine("The sentence is a pangram.");
+        }
+        else
+        {
+            Console.WriteLine("The sentence is not a pangram.");
+        }
+
+    }
+
+
+}
